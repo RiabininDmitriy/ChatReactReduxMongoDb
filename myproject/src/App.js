66bf47edad9c2dbd64ca14d41f322ba6ff02d445
RiabininDmitriy <kosmoaist@gmail.com>
2019-03-30 13:31:41 +0200

    add router

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './components/Form'
-import Chat from './components/Chat'
 import { Provider } from 'react-redux'
 import store from './store'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import createHistory from "history/createBrowserHistory";
+import About from './components/About'
+import AboutPage from './components/AboutPage'
+import Form from './components/Form'
+import Chat from './components/Chat'
 
 
-let Contanier = props => <div className={`Contanier ${props.className}`}>{props.children}</div>
+let Container = props => <div className={`Contanier ${props.className}`}>
+  <Form />
+  <Chat />
+  <About />
+</div>
+
+
+const history = createHistory();
+
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Contanier className="App">
-          <Form />
-          <Chat />
-        </Contanier>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/" component={Container} />
+            <Route  path="/about" component={AboutPage} />
+          </Switch>
+        </Router>
+
       </Provider>
     );
   }
